0e9a897a84d45b525936cf5a9030683eb4aacffa
David Ávila <david.avila.membrives@gmail.com>
2021-08-24 11:50:53 +0200

    Remove Raytracing sample

--- VulkanGen/KHRRTXHelloTriangle/Shaders/compile.bat
+++
@@ -1,3 +0,0 @@
-C:/VulkanSDK/1.2.135.0/Bin32/glslangValidator.exe -V ray-generation.rgen   -o ray-generation.spv
-C:/VulkanSDK/1.2.135.0/Bin32/glslangValidator.exe -V ray-closest-hit.rchit -o ray-closest-hit.spv
-C:/VulkanSDK/1.2.135.0/Bin32/glslangValidator.exe -V ray-miss.rmiss        -o ray-miss.spv
--- VulkanGen/KHRRTXHelloTriangle/Shaders/ray-miss.rmiss
+++
@@ -1,8 +0,0 @@
-#version 460 core
-#extension GL_EXT_ray_tracing : enable
-
-layout(location = 0) rayPayloadInEXT vec4 payload;
-
-void main() {
-  payload = vec4(0.3);
-}
